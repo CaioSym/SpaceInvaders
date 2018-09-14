4a0a6569f56813eefc1f919986ed630b1fcdd5a5
Caio Sym <CaioSym@gmail.com>
2018-09-14 00:54:13 -0300

    Create simple representation o a GraphicElement

---
+++ SpaceInvaders/GraphicElement.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceInvaders
+{
+    class GraphicElement
+    {
+        public readonly ConsoleColor Color;
+        public readonly char Character;
+
+        public GraphicElement(ConsoleColor color, char character)
+        {
+            Color = color;
+            Character = character;
+        }
+    }
+}
