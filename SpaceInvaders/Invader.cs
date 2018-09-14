422a3583a7e4e88d982adf6ec19999bb0ebf4029
Caio Sym <CaioSym@gmail.com>
2018-09-14 02:11:20 -0300

    Add the Invader class with some very basic behaviour

---
+++ SpaceInvaders/Invader.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceInvaders
+{
+    class Invader : GameObject
+    {
+        private int moveCounter = 4;
+        private Vector2 moveDirection = Vector2.RIGHT;
+
+        public Invader(Vector2 position) : base(position, new GraphicElement(ConsoleColor.Green, 'W'))
+        {
+        }
+
+        public override void OnUpdate()
+        {
+            if(--moveCounter == 0)
+            {
+                moveCounter = 4;
+                moveDirection *= -1;
+            }
+            Position += moveDirection;
+        }
+
+        
+    }
+}
