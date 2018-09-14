99c2696aecdb74263583e5b5b4b98eb2c50b7b20
Caio Sym <CaioSym@gmail.com>
2018-09-14 03:20:06 -0300

    Add some basic animation support.

---
+++ SpaceInvaders/Animation.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceInvaders
+{
+    class Animation
+    {
+        private GraphicElement[] elements;
+        private int currentIndex;
+
+        public Animation(GraphicElement[] elements)
+        {
+            this.elements = elements;
+            currentIndex = 0;
+        }
+
+        public GraphicElement NextGraphicElement()
+        {
+            GraphicElement element = elements[currentIndex++];
+            if (currentIndex >= elements.Length) { currentIndex = 0; }
+            return element;
+        }
+    }
+}
--- SpaceInvaders/Invader.cs
+++ SpaceInvaders/Invader.cs
@@ -8,23 +8,39 @@ namespace SpaceInvaders
 {
     class Invader : GameObject
     {
-        private int moveCounter = 4;
-        private Vector2 moveDirection = Vector2.RIGHT;
+        private const int MOVE_COUNT = 10;
 
-        public Invader(Vector2 position) : base(position, new GraphicElement(ConsoleColor.Green, 'W'))
+        private int moveCounter = MOVE_COUNT;
+        private Vector2 moveDirection = Vector2.RIGHT;
+        private int speed = 1;
+
+        private Animation animation = new Animation(new [] {
+            new GraphicElement(ConsoleColor.Green, 'V'),
+            new GraphicElement(ConsoleColor.Green, 'W')
+        });
+
+        public Invader(Vector2 position) : base(position, null)
         {
+            Graphics = animation.NextGraphicElement();
         }
 
         public override void OnUpdate()
         {
-            if(--moveCounter == 0)
-            {
-                moveCounter = 4;
-                moveDirection *= -1;
-            }
-            Position += moveDirection;
+            Move();
+            Graphics = animation.NextGraphicElement();
         }
 
-        
+        private void Move()
+        {
+            if (--moveCounter == 0)
+            {
+                moveCounter = MOVE_COUNT;
+                moveDirection *= -1;
+                Position += Vector2.Down;
+            }
+            Position += speed * moveDirection;
+        }
+
+
     }
 }
