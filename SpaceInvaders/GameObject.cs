17e9e0dd897d9f6eaed3df888e8e9f25511a610e
Caio Sym <CaioSym@gmail.com>
2018-09-14 00:54:42 -0300

    Create GameObject class

---
+++ SpaceInvaders/GameObject.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceInvaders
+{
+    class GameObject
+    {
+        public Vector2 Position;
+        public GraphicElement Graphics;
+
+        public GameObject(Vector2 position, GraphicElement graphics)
+        {
+            Position = position;
+            Graphics = graphics;
+        }
+
+
+    }
+}
