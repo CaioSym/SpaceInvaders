8549895fa73cb13c2e5bd6e137a4b782b8fc22e6
Caio Sym <CaioSym@gmail.com>
2018-09-15 19:55:11 -0300

    Add GameStateManager Singleton to track the GameStage

---
+++ SpaceInvaders/GameStateManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceInvaders
+{
+    class GameStateManager
+    {
+        // Singleton
+        private static GameStateManager instance;
+        public static GameStateManager Instance
+        {
+            get
+            {
+
+                if (instance == null) { instance = new GameStateManager(); }
+                return instance;
+            }
+        }
+
+        public GameState GameState = GameState.PLAYING;
+    }
+}
