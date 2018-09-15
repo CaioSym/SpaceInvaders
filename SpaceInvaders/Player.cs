e8b51298f5ffc73e75a556023bf6e596f8f98adc
Caio Sym <CaioSym@gmail.com>
2018-09-14 22:42:12 -0300

    Create the Player class

---
+++ SpaceInvaders/Player.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceInvaders
+{
+    class Player : GameObject
+    {
+        public Player(Vector2 position) : base(position, new GraphicElement(ConsoleColor.Blue, 'A'))
+        {
+        }
+
+        public void HandleKeyPressed(ConsoleKey key)
+        {
+            switch (key)
+            {
+                case ConsoleKey.A:
+                    Move(Vector2.LEFT);
+                    break;
+                case ConsoleKey.D:
+                    Move(Vector2.RIGHT);
+                    break;
+                case ConsoleKey.Enter:
+                    Fire();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void Move(Vector2 direction)
+        {
+            Position += direction;
+        }
+
+        private void Fire()
+        {
+            
+        }
+    }
+}
